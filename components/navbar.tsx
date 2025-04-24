8d2a860755569fb14706280b8643f4efa4f6f647
Nayel Abed Razi <mugwurp@gmail.com>
2025-04-24 07:10:10 +0530

    Bug fixes...

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ const navbar = () => {
   return (
     <div className='flex items-center justify-center gap-15 m-5'>
       <div className='font-bold'>
-        <h1 className='text-lg'>Engineer's Space</h1>
+        <h1 className='text-lg'>Engineer&apos;s Space</h1>
       </div>
       <div className='flex gap-2'>
         <Link href="/calculator" className='hover:text-gray-500 hover:scale-110 text-base'>Calculator</Link>
